1773bac5d28a9603f2364f98156886d83b9cc6be
Jonas Wendland <wendjon@gmx.de>
2024-04-22 11:25:35 +0200

    charackterCard function

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,24 @@
-export function CharacterCard() {}
+export function CharacterCard(image, name, status, type, episode) {
+  const ul = document.querySelector('[data-js="card-container"]');
+  const card = document.createElement("li");
+  card.classList.add("card");
+  card.innerHTML = `<div class="card__image-container">
+      <img
+      class="card__image"
+      src="${image}"
+      alt="${name}"
+    />
+    <div class="card__image-gradient"></div>
+    </div>
+    <div class="card__content">
+    <h2 class="card__title">"${name}"</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">"${status}"</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${type}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${episode}</dd>
+    </dl> </div>`;
+  ul.append(card);
+}
