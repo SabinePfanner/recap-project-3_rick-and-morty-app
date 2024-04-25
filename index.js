86765361a50f104b6ff6be909cee244807364a31
Jonas Wendland <wendjon@gmx.de>
2024-04-25 15:06:56 +0200

    nicht mergen

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
-const url = "https://rickandmortyapi.com/api/character";
+let maxPage = 42;
+let searchQuery = "";
+let page = 1;
 
-async function fetchCharacters() {
-  cardContainer.innerHTML = "";
+async function fetchCharacters(pageIndex, searchQuery) {
+  const url = `https://rickandmortyapi.com/api/character?page=${pageIndex}&name=${searchQuery}`;
   const response = await fetch(url);
   const data = await response.json();
   const characters = data.results;
+  console.log(data.info);
+  cardContainer.innerHTML = "";
+  maxPage = data.info.pages;
+  pagination.textContent = `${page}/${maxPage} `;
 
   characters.forEach((character) => {
     CharacterCard(
@@ -31,4 +34,27 @@ async function fetchCharacters() {
     );
   });
 }
-fetchCharacters();
+fetchCharacters(page, searchQuery);
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    pagination.textContent = `${page}/${maxPage}`;
+    fetchCharacters(page, searchQuery);
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    pagination.textContent = `${page}/${maxPage}`;
+    fetchCharacters(page, searchQuery);
+  }
+});
+
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = event.target.query.value;
+  page = 1;
+  fetchCharacters(page, searchQuery);
+});
